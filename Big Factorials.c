2ca607766c364ad3e1555699df305a57d14b300a
Healer <104686222+Healer36@users.noreply.github.com>
2022-05-15 17:09:38 +0600

    Update Big Factorials.c

--- Big Factorials.c
+++ Big Factorials.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
-int main()
-{
-    long long int a, i, k=1;
-    scanf("%lld", &a);
-    if(a>=20) {
+void factorial(long long int num);
+
+int main(){
+    long long int num;
+    scanf("%lld", &num);
+    
+    factorial(num);
+    
+    
+    return 0;
+}
+
+void factorial(long long int num){
+	long long int i, count=1;
+	
+    if(num>=20) {
         printf("0000\n");
     }
     else {
-        for(i=1; i<=a; i++) {
-            k=k*i;
+		scanf("%lld", &num);
+        for(i=1; i<=num; i++) {
+            count=count*i;
         }
-        if(k%10000>=0 && k%10000<=9) {
-            printf("000%lld\n", k%10000);
+        if(count%10000>=0 && count%10000<=9) {
+            printf("000%lld\n", count%10000);
         }
-        else if(k%10000>=10 && k%10000<=99) {
-            printf("00%lld\n", k%10000);
+        else if(count%10000>=10 && count%10000<=99) {
+            printf("00%lld\n", count%10000);
         }
-        else if(k%10000>=100 && k%10000<=999) {
-            printf("0%lld\n", k%10000);
+        else if(count%10000>=100 && count%10000<=999) {
+            printf("0%lld\n", count%10000);
             }
-        else if(k%10000>999) {
-        printf("%lld\n", k%10000); }
+        else if(count%10000>999) {
+        printf("%lld\n", count%10000); }
     }
-    return 0;
-}
+
+	}
